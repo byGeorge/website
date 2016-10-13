ee37649cbdd9d0313b90d3665101ec9d6a5f250b
byGeorge <george@aprillemae.com>
2016-10-13 15:50:18 -0600

    header background now does fade thing.

--- header-menu.js
+++ header-menu.js
@@ -4,44 +4,26 @@ var img = ["images/books.jpg",
 			"images/resume.jpg",
 			"images/github.jpg",
 			"images/projects.jpg"];
-
 var inc = 1; // increments through images
 var INTERVAL = 6000; // timing interval for image switch
-var fadeMe = new Image();
 
 function changeImg(){
-	var header = document.getElementById("header");
-	if (header) {
-		//fadeOut(fadeMe, 0);
-		fadeMe = header.style.backgroundImage = "url("+img[inc]+")";
-		//fadeIn(fadeMe, 100);
-	}
+	// fades and changes background
+	$('#headerimage').fadeTo("slow", 0.3, function()
+		{
+		    $(this).css("background-image", "url("+img[inc]+")");
+		}).fadeTo("slow", 1);
 	if ((inc+1) === img.length) {
 		inc=0;
 	}
 	else {
 		inc=inc+1;
 	}
-}
-/**
-function fadeOut(dark) {
-	if (dark <= 100) {
-		dark = dark - 2;
-		header.style.opacity = dark/100;
-		header.style.filter = 'alpha'(opacity=dark); // thanks, ie :/
-		setTimeout(fadeIn(dark), 100);
-	}
+	// change underlying background image
+	$('#header').css("background-image", "url("+img[inc]+")");
 }
 
-function fadeIn(dark) {
-	if (dark <= 1) {
-		dark = dark + 2;
-		header.style.opacity = dark/100;
-		header.style.filter = 'alpha'(opacity=dark);
-		setTimeout(fadeIn(dark), 100);
-	}
-} 
-**/
 function timer() {
+	$('#header').css("background-image", "url("+img[1]+")");
 	setInterval(changeImg, INTERVAL);
 }
