a3179ec30995a84ccad3686762846aa5d18549b1
byGeorge <george@aprillemae.com>
2016-10-17 22:11:31 -0600

    fades nicely now

--- header-menu.js
+++ header-menu.js
@@ -9,21 +9,20 @@ var INTERVAL = 6000; // timing interval for image switch
 
 function changeImg(){
 	// fades and changes background
-	$('#headerimage').fadeTo("slow", 0.3, function()
+	$('#headerimage').fadeTo("slow", 0, function()
 		{
 		    $(this).css("background-image", "url("+img[inc]+")");
-		}).fadeTo("slow", 1);
+		}).fadeTo("slow", .5);
 	if ((inc+1) === img.length) {
 		inc=0;
 	}
 	else {
 		inc=inc+1;
 	}
-	// change underlying background image
-	$('#header').css("background-image", "url("+img[inc]+")");
+	$('#headerfader').css("background-image", "url("+img[inc]+")");
 }
 
 function timer() {
-	$('#header').css("background-image", "url("+img[1]+")");
 	setInterval(changeImg, INTERVAL);
 }
+
